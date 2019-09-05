7529b1cf19688c1af50b029448e0f77a4618ac9e
Kevin Chau <kevin@kevinchau.com>
2019-09-04 20:40:28 -0700

    Cleanup.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
